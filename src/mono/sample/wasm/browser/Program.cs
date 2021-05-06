0a454d20c64ae1066f90e6eea7e2bc406370210c
Radek Doulik <radekdoulik@gmail.com>
2021-05-06 17:15:31 +0200

    Test exception filtering order

--- src/mono/sample/wasm/browser/Program.cs
+++ src/mono/sample/wasm/browser/Program.cs
@@ -16,7 +16,45 @@ namespace Sample
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static int TestMeaning()
         {
+            TestExcFilter();
+
             return 42;
+        }
+
+        static int answer;
+
+        static bool CheckAnswer()
+        {
+            System.Console.WriteLine($"check: {answer} == 42 => {answer == 42}");
+
+            return answer == 42;
+        }
+
+        static void TestExcFilter()
+        {
+            answer = 42;
+            try
+            {
+                Method();
+            }
+            catch (Exception) when (CheckAnswer())
+            {
+                System.Console.WriteLine($"catch in TestExcFilter, answer: {answer}");
+            }
+        }
+
+        static void Method()
+        {
+            try
+            {
+                System.Console.WriteLine("going to throw");
+                throw new Exception("Throw");
+            }
+            finally
+            {
+                System.Console.WriteLine("finally: answer = 1");
+                answer = 1;
+            }
         }
     }
 }
