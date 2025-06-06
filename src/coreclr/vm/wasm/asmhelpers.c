ae9b5ea62e126562d090982bb524b14caeac4981
Radek Doulik <radek.doulik@gmail.com>
2025-06-06 17:24:19 +0200

    Remove old files

--- src/coreclr/vm/wasm/asmhelpers.c
+++
@@ -1,9 +0,0 @@
-void __stdcall GetCurrentIP(void)
-{
-    _ASSERTE("GetCurrentIP is not implemented on wasm");
-}
-
-void __stdcall GetCurrentSP(void)
-{
-    _ASSERTE("GetCurrentSP is not implemented on wasm");
-}
